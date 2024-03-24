32e1ac786897fdce16df497892e9531faba1d4c9
Mayank Shekhawat <mayankrajshekhawat@gmail.com>
2024-03-24 14:32:24 +0530

    added the dashboard UI's basic template

--- frontend/context/userContext.jsx
+++ frontend/context/userContext.jsx
@@ -4,7 +4,7 @@ import {createContext , useState , useEffect} from 'react';
 export const UserContext = createContext({});
 
 export function UserContextProvider({children}){
-    const [user , setUser] = useState();
+    const [user , setUser] = useState(null);
     useEffect(()=>{
         if(!user){
             axios.get('/profile').then(({data})=>{
--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,67 @@
 import {useContext} from 'react';
 import { UserContext } from "../../../context/userContext"
-
+import './Dashboard.css'
 function Dashboard() {
-    const {user} = useContext(UserContext);
+    // const {user} = useContext(UserContext);
+    // console.log(user.name);
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>Welcome {user.name}</p>
+            {/* <h1>Dashboard</h1> */}
+            {/* <p>Welcome {user.name}</p> */}
+            <div className="main">
+                <div className="sidebar">
+                    <div className="Logo">LOGO HERE!</div>
+
+                    <div className="links">
+                        <div className="link">
+                            <a href="#">Dashboard</a>
+                        </div>
+                        <div className="link">
+                            <a href="#">Properties</a>
+                        </div>
+                        <div className="link">
+                            <a href="#">Clients</a>
+                        </div>
+                        <div className="link">
+                            <a href="#">Settings</a>
+                        </div>
+                    </div>
+
+                    <div className="logout">
+                        <a href="/logout">Logout</a>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <div className="header-text">
+                        <div className="text">Dashboard</div>
+                        <div className="images">
+                            <div className="search">icons</div>
+                        </div>
+                    </div>
+
+                    <div className="profileRevenue">
+                        <div className="profile"></div>
+                        <div className="revenue"></div>
+                    </div>
+
+                    <div className="boxes">
+                        <div className="cards"></div>
+                        <div className="cards"></div>
+                        <div className="cards"></div>
+                        <div className="cards"></div>
+                    </div>
+
+                    <div className="charts">
+                        <div className="monthly"></div>
+                        <div className="billings"></div>
+                    </div>
+
+                    <div className="residents"></div>
+                </div>
+
+
+            </div>
         </div>
     )
 }
