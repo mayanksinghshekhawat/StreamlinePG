1ad91fac0a56bc1330877ad65eb28ef9b9a19709
Mayank Shekhawat <mayankrajshekhawat@gmail.com>
2024-03-31 22:52:36 +0530

    resolved user login issue

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -6,10 +6,12 @@ import ChartDataLabels from "chartjs-plugin-datalabels";
 ChartJS.register(ArcElement, Tooltip, Legend,ChartDataLabels);
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
-
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 ChartJS.register(ArcElement, Tooltip, Legend);
 // import '.../assets/house.png'
 function Dashboard() {
+  const Navigate = useNavigate();
     let data= [
         {
           label: "Label 1",
@@ -60,35 +62,44 @@ function Dashboard() {
             },
           ],
         };
-    // const {user} = useContext(UserContext);
+    const {user} = useContext(UserContext);
+    const userName = user?.name || "Guest";
     // console.log(user.name);
     return (
         <div>
             {/* <h1>Dashboard</h1> */}
             {/* <p>Welcome {user.name}</p> */}
             <div className="main">
-                <div className="sidebar">
-                    <div className="Logo">LOGO HERE!</div>
+            <div className="sideBar">
+      <div className="card">
+  <div className="card-border-top">
+  </div>
+  <div className="img">
+  </div>
+  <span> {userName}</span>
+  <p className="job"> Job Title</p>
+  {/* <button> Click
+  </button> */}
+</div>
+        {/* <button className="shadow__btn">StreamlinePG</button> */}
+    
+        <div className="links">
+        <Link to="/" className="btn">Home</Link>
+          <button className="btn">Favourites</button>
+          <button className="btn">Hover me</button>
+          <button className="btn">Hover me</button>
+        </div>
 
-                    <div className="links">
-                        <div className="link">
-                            <a href="#">Dashboard</a>
-                        </div>
-                        <div className="link">
-                            <a href="/PropertyForm">Properties</a>
-                        </div>
-                        <div className="link">
-                            <a href="#">Clients</a>
-                        </div>
-                        <div className="link">
-                            <a href="#">Settings</a>
-                        </div>
-                    </div>
+        <button className="Btn">
+          <div className="sign">
+            <svg viewBox="0 0 512 512">
+              <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
+            </svg>
+          </div>
 
-                    <div className="logout">
-                        <a href="/logout">Logout</a>
-                    </div>
-                </div>
+          <div className="text">Logout</div>
+        </button>
+      </div>
 
                 <div className="content">
                     <div className="header-text">
