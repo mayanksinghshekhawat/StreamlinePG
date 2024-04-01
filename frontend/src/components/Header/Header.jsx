4841790eb0f865680a1d3277dd2e9050f4a09798
avendersharma <avendersharma1981@gamil.com>
2024-04-01 23:29:46 +0530

    added the header and footer components

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,11 +3,66 @@ import { Link } from 'react-router-dom'
 import './Header.css'
 function Header() {
   return (
-    <div><Link to="/">HOME</Link>
+    <>
+    
+    <div className="header">
+    {/* <div><Link to="/">HOME</Link>
     <Link to="/signup">Signup</Link>
     <Link to="/login">Login</Link>
+    </div> */}
+    <nav className='main-nav'>
+    <div class="right">
+           <ul>
+            <li>
+              <a href="#">buy</a>
+            </li>
+            <li>
+              <a href="#">rent</a>
+            </li>
+            <li>
+              <a href="#">mortgage</a>
+            </li>
+           </ul>
+          </div>
+
+
+          <div className='logo'>
+        <h2>
+          <span>PG</span>
+          <span>F</span>inder
+        </h2>
+      </div>
+      
+
+    <div className="menu-link">
+        <ul className='menu-link-desktop'>
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">signup</a>
+          </li>
+          <li>
+            <a href="#">login</a>
+          </li>
+          <li>
+            <a href="#">about</a>
+          </li>
+          <li>
+            <a href="#">contact us</a>
+          </li>
+        </ul>  
     </div>
+    </nav>
+    <section className="hero">
+      <h1>
+        HOMES THAT MATCH
+      </h1>
+    </section>
+    </div>
+    </>
   )
+  
 }
 
 export default Header
