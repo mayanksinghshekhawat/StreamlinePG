d86c4492a199f5b320dcd6126fd521936a930eb9
Mayank Shekhawat <mayankrajshekhawat@gmail.com>
2024-04-02 12:57:03 +0530

    improved some stylings of header section

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
+import { useSpring, animated } from 'react-spring';
 function Header() {
+  const props = useSpring({
+    from: { opacity: 0, transform: 'translateY(50px)', color: 'black' }, // Initial color is black
+    to: { opacity: 1, transform: 'translateY(0px)', color: 'white' }, // Final color is white
+    config: { duration: 500, tension: 120, friction: 20 },
+  });
+  const propsMatch = useSpring({
+    from: { opacity: 0, transform: 'translateY(50px) translateX(20px)', color: 'black' }, // Initial color is black and slight right translation
+    to: { opacity: 1, transform: 'translateY(0px) translateX(10px)', color: 'white' }, // Final color is white and slight right translation
+    config: { duration: 500, tension: 120, friction: 20 },
+  });
   return (
     <>
     
@@ -54,11 +65,12 @@ function Header() {
         </ul>  
     </div>
     </nav>
-    <section className="hero">
-      <h1>
-        HOMES THAT MATCH
-      </h1>
-    </section>
+    <animated.div className="hero-text" style={props}>
+  <h1 >Homes</h1>
+</animated.div>
+<animated.div className="hero-text2" style={propsMatch}>
+  <h1 >That Match</h1>
+</animated.div>
     </div>
     </>
   )
